9e0d93e458cbadab9070039687b3f7c77122275d
integralconstant <kevinmecard@gmail.com>
2026-09-17 12:40:30 +0900

    linear list

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("Hello, World!")
+kakao = ["가나", "다라", "마바", "사아", "자차"]
+print(kakao)
+print(kakao[0])
+print(kakao[4])
+kakao.append(None)
+print(kakao)
+kakao.append("카타")
+print(kakao)
+kakao[5] = "카타"
+print(kakao)
